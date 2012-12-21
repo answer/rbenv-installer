50da97f2dbdb70b73e6a6f02f354e614f7740ab5
sakai shunsuke <sakai@ans-web.co.jp>
2012-12-21 14:26:40 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
 dir=`pwd`
 
+echo 'git clone git://github.com/sstephenson/rbenv.git .rbenv'
 git clone git://github.com/sstephenson/rbenv.git .rbenv
+
+echo 'echo ''export PATH="'$dir'/.rbenv/bin:$PATH"'' >> .bash_profile'
 echo 'export PATH="'$dir'/.rbenv/bin:$PATH"' >> .bash_profile
+
+echo 'echo ''eval "$(rbenv init -)"'' >> .bash_profile'
 echo 'eval "$(rbenv init -)"' >> .bash_profile
+
+echo 'mkdir -p .rbenv/plugins'
 mkdir -p .rbenv/plugins
+
+echo 'cd .rbenv/plugins/'
 cd .rbenv/plugins/
+
+echo 'git clone git://github.com/sstephenson/ruby-build.git'
 git clone git://github.com/sstephenson/ruby-build.git
-cd ../..
-. .bash_profile
 
 echo
 echo
 echo "rbenv install complete."
+echo
+echo "to activate rbenv"
+echo "$ . .bash_profile"
+echo
 echo "to install ruby"
 echo "$ rbenv install <version>"
-echo "$ rbenv rehash"
 echo
 echo "to install bundler gem"
 echo "$ rbenv local <version>"
 echo "$ gem install bundler"
+echo "$ rbenv rehash"
