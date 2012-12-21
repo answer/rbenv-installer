89fa8ac00e3e74c344725ef9a97bd01eb5138b3c
sakai shunsuke <sakai@ans-web.co.jp>
2012-12-21 14:32:29 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,31 +1,34 @@
 dir=`pwd`
 
-echo 'git clone git://github.com/sstephenson/rbenv.git .rbenv'
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-
-echo 'echo ''export PATH="'$dir'/.rbenv/bin:$PATH"'' >> .bash_profile'
 echo 'export PATH="'$dir'/.rbenv/bin:$PATH"' >> .bash_profile
-
-echo 'echo ''eval "$(rbenv init -)"'' >> .bash_profile'
 echo 'eval "$(rbenv init -)"' >> .bash_profile
 
-echo 'mkdir -p .rbenv/plugins'
+git clone git://github.com/sstephenson/rbenv.git .rbenv
 mkdir -p .rbenv/plugins
-
-echo 'cd .rbenv/plugins/'
 cd .rbenv/plugins/
-
-echo 'git clone git://github.com/sstephenson/ruby-build.git'
 git clone git://github.com/sstephenson/ruby-build.git
 
 echo
 echo
 echo "rbenv install complete."
 echo
+echo "whole command lines"
+echo
+echo '$ echo ''export PATH="'$dir'/.rbenv/bin:$PATH"'' >> .bash_profile'
+echo '$ echo ''eval "$(rbenv init -)"'' >> .bash_profile'
+echo '$ git clone git://github.com/sstephenson/rbenv.git .rbenv'
+echo '$ mkdir -p .rbenv/plugins'
+echo '$ cd .rbenv/plugins/'
+echo '$ git clone git://github.com/sstephenson/ruby-build.git'
+echo
+echo
+echo "you have to execute following command line"
+echo
 echo "to activate rbenv"
 echo "$ . .bash_profile"
 echo
 echo "to install ruby"
+echo "$ rbenv install -l # list of versions"
 echo "$ rbenv install <version>"
 echo
 echo "to install bundler gem"
